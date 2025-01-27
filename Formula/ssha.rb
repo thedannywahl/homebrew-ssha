112097bb6e36cb9206024f3dd84ad29e1e886ec7
Danny Wahl <danny@iywahl.com>
2025-01-27 15:58:16 -0700

    update tests

--- Formula/ssha.rb
+++ Formula/ssha.rb
@@ -1,8 +1,8 @@
 class Ssha < Formula
   desc "Generate RFC 2307 SSHA password hashes."
   homepage "https://github.com/thedannywahl/ssha"
-  url "https://github.com/thedannywahl/ssha/archive/refs/tags/1.0.1.zip"
-  sha256 "fc5b1a854f42f6c4189aa9b3e4b477fee34b7c78bcf6672fd61ec39d57cd3409"
+  url "https://github.com/thedannywahl/ssha/archive/refs/tags/1.1.0.zip"
+  sha256 "2a712e19e50a2429f5a83764ed91e05ec6a92827d0a4bc972a77d37fb7f4a288"
   license "Apache-2.0"
 
   def install
@@ -14,12 +14,15 @@ class Ssha < Formula
     # Tests use the password "password" and salt "salt"
 
     # Ensure the binary outputs the expected hash
-    assert_equal "e1NTSEF9Yzg4ZTljNjcwNDFhNzRlMDM1N2JlZmRmZjkzZjg3ZGRlMDkwNDIxNHNhbHQ=", shell_output("#{bin}/ssha password salt").strip
+    assert_equal "{SSHA}Yzg4ZTljNjcwNDFhNzRlMDM1N2JlZmRmZjkzZjg3ZGRlMDkwNDIxNHNhbHQ=", shell_output("#{bin}/ssha password salt").strip
+
+    # Ensure the base64 encoded string is correct
+    asset_equal"Yzg4ZTljNjcwNDFhNzRlMDM1N2JlZmRmZjkzZjg3ZGRlMDkwNDIxNHNhbHQ=", shell_output("#{bin}/ssha password salt | sed 's/{SSHA}//g'").strip
 
     # Ensure the hash can be base64 decoded
-    assert_equal "{SSHA}c88e9c67041a74e0357befdff93f87dde0904214salt", shell_output("#{bin}/ssha password salt | base64 -d").strip
+    assert_equal "c88e9c67041a74e0357befdff93f87dde0904214salt", shell_output("#{bin}/ssha password salt | sed 's/{SSHA}//g' | base64 -d").strip
 
     # Ensure the sha1sum matches the expected hash
-    assert_equal "c88e9c67041a74e0357befdff93f87dde0904214", shell_output("#{bin}/ssha password salt | base64 -d | sed 's/{SSHA}//g; s/salt//g").strip
+    assert_equal "c88e9c67041a74e0357befdff93f87dde0904214", shell_output("#{bin}/ssha password salt | sed 's/{SSHA}//g' | base64 -d | sed 's/salt//g'").strip
   end
 end
